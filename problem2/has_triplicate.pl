ab3178f5a1e1dae7c0f7b7573ef3c61da64b3060
Shreyans Jain <shreyansj551@gmail.com>
2019-10-10 12:15:08 +0530

    has_duplicate implemented

--- problem2/has_triplicate.pl
+++ problem2/has_triplicate.pl
@@ -1,2 +1,14 @@
-has_triplicate([H|T],N) :- 
-  
+member([H|_], H).
+
+member([_|T], X) :-
+  member(T,X).
+
+duplicate([H|T], H) :- 
+  member(T,H).
+
+duplicate([_|T], X) :-
+  duplicate(T,X).
+
+loop([H|T]) :-
+  member(T,H), write(H) ;
+  loop(T).
