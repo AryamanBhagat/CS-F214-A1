c96e984d2990f6250bf3afed8216ad620792b2c9
thepranavgupta <thepranavgupta@gmail.com>
2019-10-10 10:54:30 +0530

    added same edge functionality to distance calculation in graph.pl

---
+++ pset-1/graph.pl
@@ -0,0 +1,31 @@
+edge(a,b).
+edge(b,c).
+edge(c,d).
+edge(d,a).
+
+% ----predicate to check if 2 nodes are connected---- %
+isConnected(X,Y) :-
+        edge(X,Y) ;
+        (edge(X,Z) , isConnected(Z,Y)).
+
+
+% ----predicate to calculate distance---- %
+distance(X, Y, SoFar, Distance) :-
+        isSame(X, Y),
+        Distance is SoFar
+
+distance(X,Y,SoFar,Distance) :-
+        edge(X,Y),
+        Distance is SoFar + 1.0.
+
+distance(X,Y,SoFar,Distance) :-
+        edge(X,Z),
+        DistanceToZ is SoFar + 1,
+        distance(Z,Y,DistanceToZ,Distance).
+
+%--same edge case--%
+isSame(X, Y):-
+  X == Y.
+
+% ----wrapper predicate---- %
+distance(X,Y,Distance) :- distance(X,Y,0,Distance).
