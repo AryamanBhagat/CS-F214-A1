0909f1568c4e8605293d1eedb2ad1de798729aed
Shreyans Jain <shreyansj551@gmail.com>
2019-10-10 12:31:16 +0530

    is_triplicate done

--- problem2/has_triplicate.pl
+++ problem2/has_triplicate.pl
@@ -1,3 +1,12 @@
+% -------- Test Cases ---------
+% [1,1,1]
+% [1,1,2,1]
+% [1,1,1,1]
+% [1,1,1,1,2,3]
+% [1,1,1,1,2,2,2]
+% [1,2,2,2,1,1,1,3]
+% ------------------------------
+
 member([H|_], H).
 
 member([_|T], X) :-
@@ -9,6 +18,14 @@ duplicate([H|T], H) :-
 duplicate([_|T], X) :-
   duplicate(T,X).
 
+% loop([H|T]) :-
+%   member(T,H), write(H) ;
+%   loop(T).
+
 loop([H|T]) :-
-  member(T,H), write(H) ;
+  member(T,H) ;
   loop(T).
+
+is_triplicate([H|T]) :- 
+  duplicate(T, H), write(H) ;
+  is_triplicate(T).
