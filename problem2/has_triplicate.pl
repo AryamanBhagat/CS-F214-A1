0f4664b498d007f9b35fd8aac89b3ed8a04472e7
Harsh <sulakheharsh@gmail.com>
2019-10-11 01:55:29 +0530

    Took care of cases with repetitions in the output

--- problem2/has_triplicate.pl
+++ problem2/has_triplicate.pl
@@ -7,10 +7,16 @@
 % [1,2,2,2,1,1,1,3]
 % ------------------------------
 
+% ---- member(List,A) ---- 
+% member/2 evaluates to True when A is a member of List
+
 member([H|_], H).
 
 member([_|T], X) :-
   member(T,X).
+
+% ---- duplicate(List,A) ---- 
+% duplicate/2 evaluates to True when A appears at least twice in List
 
 duplicate([H|T], H) :- 
   member(T,H).
@@ -19,13 +25,24 @@ duplicate([_|T], X) :-
   duplicate(T,X).
 
 % loop([H|T]) :-
-%   member(T,H), write(H) ;
-%   loop(T).
+%  member(T,H), write(H) ;
+%  loop(T).
 
-loop([H|T]) :-
-  member(T,H) ;
-  loop(T).
+% loop([H|T]) :-
+%  member(T,H) ;
+%  loop(T).
 
-is_triplicate([H|T]) :- 
-  duplicate(T, H), write(H) ;
-  is_triplicate(T).
+% ---- is_triplicate(List,A) ----
+% is_triplicate/2 evaluates to True if A appears at least thrice in List and subsequently prints A
+
+is_triplicate([H|T],U) :- 
+  /+member(H,U),
+  duplicate(T, H),
+  write(H),
+  append(H,U);
+  is_triplicate(T,U).
+
+% ---- has_triplicate(List) ----
+% wrappper function to is_triplicate/2 which prints all elements of List that appear at least thrice within it.
+has_triplicate(X) :- 
+  is_triplicate(X,[]).
