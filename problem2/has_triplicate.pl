3e1c985215a7f672b7dd541f31cc20b5afd02b9e
Harsh <sulakheharsh@gmail.com>
2019-10-13 00:44:28 +0530

    Corrected append arity issue

--- problem2/has_triplicate.pl
+++ problem2/has_triplicate.pl
@@ -7,6 +7,11 @@
 % [1,2,2,2,1,1,1,3]
 % ------------------------------
 
+append([],L,L).
+
+append([H|L1],L2,[H|L3]) :- 
+  append(L1,L2,L3).
+
 % ---- member(List,A) ---- 
 % member/2 evaluates to True when A is a member of List
 
@@ -24,14 +29,6 @@ duplicate([H|T], H) :-
 duplicate([_|T], X) :-
   duplicate(T,X).
 
-% loop([H|T]) :-
-%  member(T,H), write(H) ;
-%  loop(T).
-
-% loop([H|T]) :-
-%  member(T,H) ;
-%  loop(T).
-
 % ---- is_triplicate(List,A) ----
 % is_triplicate/2 evaluates to True if A appears at least thrice in List and subsequently prints A
 
@@ -39,8 +36,8 @@ is_triplicate([H|T],U) :-
   \+member(H,U),
   duplicate(T, H),
   write(H),
-  append(H,U);
-  is_triplicate(T,U).
+  append(H,U,X);
+  is_triplicate(T,X).
 
 % ---- has_triplicate(List) ----
 % wrappper function to is_triplicate/2 which prints all elements of List that appear at least thrice within it.
