3f4330727a407b59f6385667795de5bbe231535b
thepranavgupta <thepranavgupta@gmail.com>
2019-10-13 17:56:11 +0530

    anonymous var at line 9 in sublist

--- problem2/sublist.pl
+++ problem2/sublist.pl
@@ -1,13 +1,13 @@
 append([],L,L).
 
-append([H|L1],L2,[H|L3]) :- 
+append([H|L1],L2,[H|L3]) :-
   append(L1,L2,L3).
 
 prefix(P,L) :- append(P,_,L).
 suffix(P,L) :- append(_,P,L).
 
-sublist([],L).
+sublist([],_).
 
-sublist(A,L) :- 
-  suffix(X,L), 
+sublist(A,L) :-
+  suffix(X,L),
   prefix(A,X).
